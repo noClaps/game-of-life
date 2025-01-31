f67507fd1ce580191cc330be56952eb97d2c4773
noClaps <git@zerolimits.dev>
2025-01-31 10:33:29 +0000

    build: Use Bun for build and dev scripts

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -1,32 +1,12 @@
-import type { ServeOptions } from "bun";
-import fs from "node:fs";
+import html from "../src/index.html";
 
-const options: ServeOptions = {
-  async fetch({ url }) {
-    const path = new URL(url).pathname;
-
-    switch (path) {
-      case "/":
-        return new Response(Bun.file("src/index.html"));
-      case "/style.css":
-        return new Response(Bun.file("src/style.css"));
-      case "/script.ts":
-        const script = await Bun.build({
-          entrypoints: ["src/script.ts"],
-        }).then((bo) => bo.outputs[0].text());
-        return new Response(script, {
-          headers: { "content-type": "application/javascript" },
-        });
-      default:
-        return new Response("Not found", { status: 404 });
-    }
+const server = Bun.serve({
+  static: {
+    "/": html,
   },
-};
-
-const server = Bun.serve(options);
-console.log(`Server started at ${server.url}`);
-
-fs.watch("src", () => {
-  server.reload(options);
-  console.log("Reloaded");
+  fetch() {
+    return new Response("Not found", { status: 404 });
+  },
 });
+
+console.log(`Server started at ${server.url}`);
